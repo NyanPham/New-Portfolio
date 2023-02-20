0a171148e3d6c2bd3d700b1ec8e34e2e500159a8
Pham Thanh Nhan <phamthanhnhanussh@gmail.com>
2023-02-20 08:24:23 +0700

    minor changes

--- dist/threeBg.328beae8.js
+++ dist/threeBg.328beae8.js
@@ -36305,7 +36305,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60001" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59943" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
